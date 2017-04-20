70b4f46afc99003718760855b2b92e93045b5f18
Stoyan Stoyanov <stoyan.stoyanov@mentormate.com>
2017-04-20 17:22:55 +0300

    added icon shapes enum

---
+++ springtoolz/springtoolz/IconShapes.h
@@ -0,0 +1,16 @@
+//
+//  IconShapes.h
+//  SpringToolz
+//
+//  Created by Stoyan Stoyanov on 4/20/17.
+//  Copyright © 2017 Stoyan Stoyanov. All rights reserved.
+//
+
+typedef NS_ENUM(NSInteger, IconShapes) {
+    IconShapeDefault,
+    IconShapeCircle,
+    IconShapeGearWheel1,
+    IconShapeGearWheel2,
+    IconShapeGearWheel3,
+    CircleRadiusDeviation
+};
