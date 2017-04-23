c9e2eca542c518eb4ad2a64d4bd83899a858a6ee
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2017-04-23 22:05:29 +0300

    added dummy implementations for SBIconView and SBIconImageView in order to be able to build again the xcode project. removed one unused method

---
+++ SpringToolzSampleProject/SpringToolzSampleProject/DummyImplementations.m
@@ -0,0 +1,17 @@
+//
+//  DummyImplementations.m
+//  SpringToolzSampleProject
+//
+//  Created by Stoyan Stoyanov on 4/23/17.
+//  Copyright © 2017 Stoyan Stoyanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "SBIconView.h"
+
+@implementation SBIconView
+@end
+
+
+@implementation SBIconImageView
+@end
--- springtoolz/UIView+Shape.m
+++ springtoolz/UIView+Shape.m
@@ -44,8 +44,4 @@
     }
 }
 
-- (void)applyMask {
-    
-}
-
 @end
