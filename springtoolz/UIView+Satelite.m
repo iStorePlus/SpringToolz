cb71a54e20a6ea35a35476acf968604f101ad44e
Stoyan Stoyanov <stoqn4opm>
2017-04-25 18:14:39 +0300

    some progress with satellites

--- springtoolz/UIView+Satelite.m
+++ springtoolz/UIView+Satelite.m
@@ -14,11 +14,10 @@
 // self will have superview for sure
 - (void)addSatellite {
     UIView *satelliteView = [[UIView alloc] initWithFrame:self.superview.frame];
-//    satelliteView.backgroundColor = [UIColor redColor];
     [satelliteView addSatelliteShapeLayer];
-    [self.superview addSubview:satelliteView];
+    [self.superview.superview addSubview:satelliteView];
     [satelliteView runSpinAnimation];
-    
+    satelliteView.alpha = 0.7;
 }
 
 
@@ -27,7 +26,7 @@
     
     CGPoint initialSatelliteLocation = CGPointMake(self.frame.size.width * 0.1, self.frame.size.height * 0.1);
     
-    UIBezierPath *satelite = [UIBezierPath bezierPathWithArcCenter:initialSatelliteLocation radius:2.5 startAngle:0 endAngle:2 * M_PI clockwise:YES];
+    UIBezierPath *satelite = [UIBezierPath bezierPathWithArcCenter:initialSatelliteLocation radius:3.5 startAngle:0 endAngle:2 * M_PI clockwise:YES];
     [shape setFillColor:[UIColor whiteColor].CGColor];
     shape.path = satelite.CGPath;
     [self.layer addSublayer:shape];
@@ -35,11 +34,13 @@
 
 - (void) runSpinAnimation {
     CABasicAnimation* rotationAnimation;
+    CGFloat toValue = arc4random_uniform(4) >= 2 ? M_PI : -M_PI;
     rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0];
-    rotationAnimation.duration = 5;
+    rotationAnimation.toValue = [NSNumber numberWithFloat: toValue];
+    rotationAnimation.duration = 3 + arc4random_uniform(10);
     rotationAnimation.cumulative = YES;
     rotationAnimation.repeatCount = HUGE_VALF;
+    rotationAnimation.timeOffset = arc4random_uniform(4);
     [self.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
 }
 
